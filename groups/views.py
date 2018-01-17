585c06a540ff9a9a0f423947c687c143695a2176
Michal Bicki <michal_bicki@epam.com>
2018-01-17 16:30:36 +0100

    post & groups models and templates created

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
-
+from django.views import generic
+from groups.models import Group, GroupMember
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+import misaka as m
+from groups.models import Group
 
 # Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *arg, **kwargs):
+        self.message_html = m.html(self.message)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('posts:single',
+                        kwargs={'username':self.user.username,
+                                'pk':self.pk})
+    
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user', 'message']
